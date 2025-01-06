537487de08b112289271c477a172eef2db40e9a8
m-lm <31556425+m-lm@users.noreply.github.com>
2025-01-05 22:08:50 -0600

    add case normalization and filter nonparticipating character entities; apply relationship extraction and visualization to all plays; convert lowercase normalization to original case at end

--- src/relationships.py
+++ src/relationships.py
@@ -14,80 +14,99 @@ import itertools
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("spacytextblob")
 ruler = nlp.add_pipe("entity_ruler", before="ner")
-df = pd.read_csv("data/plays/Macbeth.csv")
+plays = sorted(os.listdir("data/plays"))
 
-# Get list of normalized character names to add to NER ruleset
-characters = [name for name in df["character"].unique().tolist() if name != "All"]
-original_character_map = {name.lower(): name for name in characters}
-characters_normalized = list(map(str.lower, characters))
-patterns = [{"label": "PERSON", "pattern": name} for name in characters]
-ruler.add_patterns(patterns)
+# Read each play CSV and extract relationships
+for play in plays:
+    if play[0].isalpha():
+        filename = os.path.join("data/plays", play)
+    else:
+        continue
 
-# Extract nonempty entity sets for sentence texts
-df["entities"] = df["text"].apply(lambda x: [ent.text.translate(str.maketrans("", "", string.punctuation)) for ent in nlp(x).ents if ent.label_ == "PERSON"]).to_list()
-df_filtered = df[df["entities"].apply(len) > 0]
+    df = pd.read_csv("data/plays/Macbeth.csv")
 
-# Per scene, get scene character cast and first check for explicit mentions.
-# Then, move on to implicit relations from scene associations, making sure not to double up
-# if a speaker directly addresses a fellow scene participant
-scenes = df.groupby(["act", "scene"])
-explicit_relation_counts = Counter()
-implicit_relation_counts = Counter()
-for (act, scene), lines in scenes:
-    scene_chars = [name for name in lines["character"].unique() if name != "All"]
-    associations = [] # used to keep track of explicit relations in a scene when checking implicit relations
+    # Get list of normalized character names to add to NER ruleset
+    characters = [name for name in df["character"].unique().tolist() if name != "All"]
+    original_character_map = {name.lower(): name for name in characters}
+    characters_normalized = list(map(str.lower, characters))
+    patterns = [{"label": "PERSON", "pattern": name} for name in characters]
+    ruler.add_patterns(patterns)
 
-    # Organize the relationships pairwise according to a commutative/undirected (1st party, 2nd party) order
-    # This is for explicit mentions by a speaker
-    for _, row in lines.iterrows():
-        mentioned = row["entities"]
-        speaker = row["character"]
+    # Extract nonempty entity sets for sentence texts
+    df["entities"] = df["text"].apply(lambda x: [ent.text.translate(str.maketrans("", "", string.punctuation)) for ent in nlp(x).ents if ent.label_ == "PERSON"]).to_list()
+    df_filtered = df[df["entities"].apply(len) > 0]
 
-        # Skip if the speaker is "All"; only want individuals
-        if speaker == "All":
-            continue
+    # Per scene, get scene character cast and first check for explicit mentions.
+    # Then, move on to implicit relations from scene associations, making sure not to double up
+    # if a speaker directly addresses a fellow scene participant
+    scenes = df.groupby(["act", "scene"])
+    explicit_relation_counts = Counter()
+    implicit_relation_counts = Counter()
+    for (act, scene), lines in scenes:
+        scene_chars = [name for name in lines["character"].unique() if name != "All"]
+        associations = [] # used to keep track of explicit relations in a scene when checking implicit relations
 
-        for mention in mentioned:
-            if speaker != mention and mention != "All":
-                ordered_pair = (speaker, mention)
-                unordered_pair = tuple(sorted((speaker, mention)))
+        # Organize the relationships pairwise according to a commutative/undirected (1st party, 2nd party) order
+        # From here, lower() is used for name normalization. At the end the character names are mapped back to proper case.
+        # This is for explicit mentions by a speaker
+        for _, row in lines.iterrows():
+            mentioned = row["entities"]
+            speaker = row["character"].lower()
+
+            # Skip if the speaker is "All"; only want individuals
+            if speaker == "all":
+                continue
+
+            # Iterate through mentioned entities from sentence text. Note that these entities are normalized to lowercase.
+            for mention in list(map(str.lower, mentioned)):
+                if speaker != mention and mention != "all":
+                    ordered_pair = (speaker, mention)
+                    unordered_pair = tuple(sorted((speaker, mention)))
+                    associations.append(unordered_pair)
+                    explicit_relation_counts[unordered_pair] += 1
+
+        # Factor in implicit relationships via scene associations. Character within the same scene likely are related.
+        for combo in itertools.combinations(list(map(str.lower, scene_chars)), 2):
+            ordered_pair = combo
+            unordered_pair = tuple(sorted(combo))
+            # Prioritize explicit mentions over implicit scene associations
+            if unordered_pair not in associations:
                 associations.append(unordered_pair)
-                explicit_relation_counts[unordered_pair] += 1
+                implicit_relation_counts[unordered_pair] += 1
 
-    # Factor in implicit relationships via scene associations. Characters within the same scene likely are related.
-    for combo in itertools.combinations(scene_chars, 2):
-        ordered_pair = combo
-        unordered_pair = tuple(sorted(combo))
-        # Prioritize explicit mentions over implicit scene associations
-        if unordered_pair not in associations:
-            associations.append(unordered_pair)
-            implicit_relation_counts[unordered_pair] += 1
+    total_counts = explicit_relation_counts + implicit_relation_counts
+    # Convert lowercase normalized names to original proper case and check if gathered names are in the list of play characters who participate
+    # Note this primarily only works for plays as we have a convenient list of characters in the play and the worldbuilding does not cross this boundary.
+    # However, longer more complex works like epic fantasy or scifi should include those nonpresent 3rd parties mentioned even if they do not actively participate. This is for worldbulding reasons.
+    total_counts = {(original_character_map[a], original_character_map[b]): count for (a, b), count in total_counts.items() if a in original_character_map and b in original_character_map}
+    for key, _ in sorted(total_counts.items(), key=lambda item: item[1], reverse=False):
+        print("KEY: ", key)
+        print("TOTAL: ", total_counts[key])
+        # for explicit/implicit lookups, lower() the tuple values
+        print("EXPLICIT: ", explicit_relation_counts[(key[0].lower(), key[1].lower())])
+        print("IMPLICIT: ", implicit_relation_counts[(key[0].lower(), key[1].lower())])
+        print("\n"*2)
 
-total_counts = explicit_relation_counts + implicit_relation_counts
-for key, _ in sorted(total_counts.items(), key=lambda item: item[1], reverse=False):
-    print("KEY: ", key)
-    print("TOTAL: ", total_counts[key])
-    print("EXPLICIT: ", explicit_relation_counts[key])
-    print("IMPLICIT: ", implicit_relation_counts[key])
-    print("\n"*2)
+    print(scenes["character"].unique()) # print each scene and their explicit entities mentioned
 
-print(scenes["character"].unique()) # print each scene and their explicit entities mentionec
+    # Visualize character relations as signed weighed graph
+    G = nx.DiGraph()
 
-# Visualize character relations as signed weighed graph
-G = nx.DiGraph()
+    for relation, weight in total_counts.items():
+        G.add_edge(relation[0], relation[1], weight=weight)
 
-for relation, weight in total_counts.items():
-    G.add_edge(relation[0], relation[1], weight=weight)
+    viz = Network(
+        notebook=True, 
+        cdn_resources="remote",
+        neighborhood_highlight=True,
+        select_menu=True,
+        filter_menu=True,
+        )
+    for node in G.nodes():
+        viz.add_node(node, label=node, size=G.degree(node))
+    viz.from_nx(G)
 
-viz = Network(
-    notebook=True, 
-    cdn_resources="remote",
-    neighborhood_highlight=True,
-    select_menu=True,
-    filter_menu=True,
-    )
-viz.toggle_physics(True)
-for node in G.nodes():
-    viz.add_node(node, label=node, size=G.degree(node))
-viz.from_nx(G)
-viz.show("viz/emonet.html")
+    play_title = play[:play.find(".")]
+    viz.show(f"viz/{play_title}.html")
+    print(f"{play_title} done processing...")
+    break
