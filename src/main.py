d6f6dc6b68ce4bd28a3b2a70a0ecb7bf3d5f1dcd
m-lm <31556425+m-lm@users.noreply.github.com>
2025-04-22 00:09:31 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -5,16 +5,19 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--op", choices=["preprocess", "relationships", "all"], nargs="?", const="all", default="all")
+    parser.add_argument("--op", choices=["preprocess", "relationships", "visualization", "all"], nargs="?", const="all", default="all")
     args = parser.parse_args()
 
     if args.op == "all":
         preprocess.preprocess()
         relationships.extract_relationships()
+        visualization.generate_visualization()
     elif args.op == "preprocess":
         preprocess.preprocess()
     elif args.op == "relationships":
         relationships.extract_relationships()
+    elif args.op == "visualization":
+        visualization.generate_visualization()
 
 if __name__ == "__main__":
     main()
--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,24 @@
-import relationships
 import ast
-import dash
-import plotly
 import networkx as nx
 import json
+
+def recreate_graph(relations):
+    G = nx.DiGraph()
+    for rel, weight in relations.items():
+        pair = ast.literal_eval(rel)
+        G.add_edge(pair[0], pair[1], weight=weight)
+    return G
+
+def generate_visualization():
+    play_title = "A Midsummer Night's Dream"
+    with open(f"exports/snapshots/{play_title} Temporal-Snapshots.json") as f:
+        snapshots = json.load(f)
+
+    temporal_dict = {}
+    for scene, relations in snapshots.items():
+        temporal_dict[scene] = recreate_graph(relations)
+
+    # TODO: need start and end attributes for gexf files to enable dynamic graphs in Gephi
+    nx.write_gexf(G, f"exports/gexf/{play_title}.gexf")
+    print(f"{play_title} exported to dynamic gexf")
+    
